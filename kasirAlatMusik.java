162aa0da9150351d2d9a6a4748cca8f841599a37
krisnabimantoro <krisnabmntr@gmail.com>
2024-02-04 10:56:17 +0700

    first commit

---
+++ kasirAlatMusik.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class kasirAlatMusik {
+
+  static Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    managePerson.main(args);
+    item.main(args);
+    kasir.main(args);
+  }
+}
